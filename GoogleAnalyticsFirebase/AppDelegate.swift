e8631e6a2d7867be770c1c89e2f1ee9a47cc0d1e
Mathieu Lima <mathieu.lima@gmail.com>
2020-03-27 18:19:17 +0100

    disable Firebase automatic screen reporting

--- GoogleAnalyticsFirebase/AppDelegate.swift
+++ GoogleAnalyticsFirebase/AppDelegate.swift
@@ -24,7 +24,6 @@ import FirebaseInstanceID
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
         FirebaseApp.configure()
         
         return true
